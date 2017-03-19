435525384283364598899f645cd7ff5d476acd00
Fiser <fiser12@icloud.com>
2017-03-19 02:34:13 +0100

    Menos fallos ahora

--- crackerSecuencial.c
+++ crackerSecuencial.c
@@ -4,28 +4,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+
 unsigned char* fromDeci(char alpha[], int base, int inputNum, int length)
 {
-    unsigned char * devolver = (unsigned char *)calloc(length, sizeof(unsigned char));
+    unsigned char * devolver = (unsigned char *)calloc(length-1, sizeof(unsigned char));
+    int num = inputNum;
     int i = 0;
     do{
-        int index = inputNum%base;
+        int index = num%(base+1);
         devolver[i] = alpha[index];
-        inputNum = inputNum/base;
+        num = num/base;
         i++;
-    }while(i!=length);
-    devolver[i] = inputNum;
+    }while(num>=base);
+    devolver[i] = alpha[num%(base+1)];
 
     return devolver;
 }
 int main(int argc, const char* argv[]) {
     unsigned char alpha[] = "01231456789ABCDEF";
-    int lenKey = 3;
+    int lenKey = 2;
     int lenAlpha = strlen(alpha)-1;
     int i = 0;
-    for(i = 0; i<lenKey^(lenAlpha); i++){
+    int valor = pow(lenAlpha, lenKey);
+    for(i = 0; i<valor; i++){
         unsigned char * result = fromDeci(alpha, lenAlpha, i, lenKey);
-        printf("%s\n", result);
+        printf("%d de %d es %s\n", valor, i, result);
     }
     return 0;
 }
