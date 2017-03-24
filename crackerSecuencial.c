47db429465d5d26d9850c84be350eb425f929c58
Fiser <fiser12@icloud.com>
2017-03-24 16:16:11 +0100

    Arreglado el fallo :)

--- crackerSecuencial.c
+++ crackerSecuencial.c
@@ -1,6 +1,5 @@
 //
 // Created by fiser on 10/03/17.
-// Parametros (hash, alfaveto, mínimo de clave, máximo de clave)
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -16,19 +15,20 @@ unsigned char* fromDeci(unsigned char alpha[], int base, int inputNum, int lengt
         devolver[i] = alpha[index];
         num = num/base;
         i--;
-    }while(num>=base);
+    }while(i==1);
     devolver[i] = alpha[num];
 
     return devolver;
 }
 int main(int argc, const char* argv[]) {
     unsigned char alpha[] = "0123456789ABCDEF";
-    int lenKey = 2;
+    int lenKey = 3;
     int lenAlpha = strlen(alpha);
     int i = 0;
     int valor = pow(lenAlpha, lenKey);
     for(i = 0; i<valor; i++){
         unsigned char * result = fromDeci(alpha, lenAlpha, i, lenKey);
+        free(result);
         printf("%d de %d es %s\n", valor, i, result);
     }
     return 0;
