f9a023177cbd7c7ebd8d070113f04991c9d5c9e5
Fiser <fiser12@icloud.com>
2017-03-24 22:14:26 +0100

    Arreglado el fallo :)

--- crackerSecuencial.c
+++ crackerSecuencial.c
@@ -7,29 +7,32 @@
 
 unsigned char* fromDeci(unsigned char alpha[], int base, int inputNum, int length)
 {
-    unsigned char * devolver = (unsigned char *)calloc(length-1, sizeof(unsigned char));
+    unsigned char * devolver = (unsigned char *)calloc(length, sizeof(unsigned char));
     int num = inputNum;
-    int i = length-1;
-    do{
+    int i = length - 1;
+    while(i!=0){
         int index = num%(base);
         devolver[i] = alpha[index];
         num = num/base;
         i--;
-    }while(i==1);
+    }
     devolver[i] = alpha[num];
 
     return devolver;
 }
 int main(int argc, const char* argv[]) {
     unsigned char alpha[] = "0123456789ABCDEF";
-    int lenKey = 3;
+    int lenKeyMax = 3;
+    int lenKeyMin = 2;
     int lenAlpha = strlen(alpha);
     int i = 0;
-    int valor = pow(lenAlpha, lenKey);
-    for(i = 0; i<valor; i++){
-        unsigned char * result = fromDeci(alpha, lenAlpha, i, lenKey);
-        free(result);
-        printf("%d de %d es %s\n", valor, i, result);
+    for(int j = lenKeyMin; j<=lenKeyMax; j++) {
+        int valor = pow(lenAlpha, j);
+        for(i = 0; i<valor; i++){
+            unsigned char *result = fromDeci(alpha, lenAlpha, i, j);
+            printf("%d de %d es %s\n", valor, i, result);
+            free(result);
+        }
     }
     return 0;
 }
