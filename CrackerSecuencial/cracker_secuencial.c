963eeb031c8e9e4676de9cc3d35a6aa18f6a1706
Fiser <Fiser@MacBook-Pro-de-Fiser.local>
2017-03-30 16:43:35 +0200

    Arreglado el fallo :)

--- CrackerSecuencial/cracker_secuencial.c
+++ CrackerSecuencial/cracker_secuencial.c
@@ -59,9 +59,9 @@ int main(int argc, const char *argv[]) {
                 printf("%s \t | \t %s\n", buffer, ejemplo_diggest);
                 printf("\n");
                 printf("%d | %d \n", strlen(ejemplo_diggest), strlen(buffer));
+                int comparacion = strcmp((unsigned char *) ejemplo_diggest, candidate_diggest);
+                printf("Comparacion: %d\n", comparacion);
             }
-            int comparacion = strcmp((unsigned char *) ejemplo_diggest, candidate_diggest);
-            //printf("Comparacion: %d\n", comparacion);
             free(candidato);
             free(candidate_diggest);
         }
