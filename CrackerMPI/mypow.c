57227c5ec8e07d00ff7b34e17bf158e102c13d75
Aitor <aitor@aaV5Rotia.acer>
2017-04-24 20:34:20 +0200

    Comenzando con el cracker en paralelo con MPI

---
+++ CrackerMPI/mypow.c
@@ -0,0 +1,9 @@
+/* This is our own implementation of the power function. Since we are not interested in working with doubles and that we want to avoid casting values from one type to another, we have implemented our own power function */
+unsigned long long int mypow(unsigned long long int base, int exponente) {
+	int i;
+	unsigned long long int resul = 1ULL;
+	for (i = 0; i < exponente; i++) {
+		resul *= base;
+	}
+	return resul;
+}
