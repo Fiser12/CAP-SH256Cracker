4cef25abe8ebdda14e22508f5a7201c659930c81
fiser <fiser12@icloud.com>
2017-04-28 00:17:48 +0200

    Compilados añadidos

--- CrackerMPI/cracker.c
+++ CrackerMPI/cracker.c
@@ -5,6 +5,7 @@
 #include <unistd.h> // para el 'getOpt'
 #include "mypow.h" // Implementacion de nuestra propia version de 'pow'
 #include "hasher/sha256.h" // Implementacion del hasher. En nuestro caso: SHA256
+#include <time.h>
 
 #define MIN 1
 #define MAX 4
@@ -33,7 +34,7 @@ unsigned char *cambioBase(unsigned char alpha[], unsigned long long num, int key
 }
 
 /* Este es el programa principal. Recibimos los cuatro parametros antes descritos (3 de ellos opcionales, el <hash> obligatorio!) */
-int main(int argc, char *argv[]) {   
+int main(int argc, char *argv[]) {
 
     MPI_Status status; // Muestra el estado de la conexion (paralelizacion con MPI)
     unsigned char *alphabet = NULL; // Alfabeto a utilizar para probar contra el <hash>
@@ -44,62 +45,67 @@ int main(int argc, char *argv[]) {
     ///////// START GETOPT IMPLEMENTATION: //////////////////////////////////////////////
     int c; // Caracter de opcion que parseamos (si existen...)
     opterr = 0; // If you set this variable to zero, getopt does not print any messages, but it still returns the character '?' to indicate an error
-
+    int size = 1;
     // Parseamos los parametros de entrada, busamos una '-a', '-n' y/o '-m':
-    while ((c = getopt (argc, argv, "a:n:m:")) != -1)
-    switch (c) {
-      case 'a':
-        alphabet = optarg;
-        break;
-      case 'n':
-        lenKeyMin = atoi(optarg);
-        break;
-      case 'm':
-        lenKeyMax = atoi(optarg);
-        break;
-      case '?':
-	// Si 'getOpt' no reconoce un caracter de opcion dado, guarda dicho caracter en 'optopt' y devuelve '?'
-        // Si 'getOpt' encuentra un caracter de opcion reconocible, pero no se encuentra valor (missing) del parametro, entonces guarda dicho
-	// caracter en 'optopt' y devuelve '?'.
-        if (optopt == 'a' || optopt == 'n' || optopt == 'm')
-          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        else if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
-	break;
-      default:
-	fprintf (stderr, "A problem was detected. No option character written, nor unknown option.");
-        exit(EXIT_FAILURE); // Salimos de la ejecucion del cracker. Algo raro ha debido de pasar.
+    while ((c = getopt(argc, argv, "a:n:m:s:")) != -1)
+        switch (c) {
+            case 'a':
+                alphabet = optarg;
+                break;
+            case 'n':
+                lenKeyMin = atoi(optarg);
+                break;
+            case 'm':
+                lenKeyMax = atoi(optarg);
+                break;
+            case 's':
+                size = atoi(optarg);
+                break;
+            case '?':
+                // Si 'getOpt' no reconoce un caracter de opcion dado, guarda dicho caracter en 'optopt' y devuelve '?'
+                // Si 'getOpt' encuentra un caracter de opcion reconocible, pero no se encuentra valor (missing) del parametro, entonces guarda dicho
+                // caracter en 'optopt' y devuelve '?'.
+                if (optopt == 'a' || optopt == 'n' || optopt == 'm')
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint(optopt))
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+                break;
+            default:
+                fprintf(stderr, "A problem was detected. No option character written, nor unknown option.");
+                exit(EXIT_FAILURE); // Salimos de la ejecucion del cracker. Algo raro ha debido de pasar.
+        }
+
+    // Miramos si queda algun parámetro sin caracter de opcion (es decir, un parametro suelto sin precedente). En este caso, deberia ser el hash.
+    if (optind + 1 == argc) {
+        // hay hash!
+        ejemplo_diggest = argv[optind];
+        // Lo convertimos a UPPER CASE por si acaso:
+        int p = 0;
+        while (ejemplo_diggest[p] != '\0') {
+            ejemplo_diggest[p] = toupper(ejemplo_diggest[p]);
+            p++;
+        }
+    } else {
+        // Si no hay más parametros o hay más de uno, mostramos por pantalla una alerta al usuario.
+        printf("Error, usage: %s [-a \"ALPHABET\"] [-n MIN] [-m MAX] <hash>\n", argv[0]);
+        exit(EXIT_FAILURE); // no seguimos ejecutando
+    }
+    // Si el usuario no ha introducido los parametros opcionales y solo el hash como tal. Inicializamos a valores por defecto (DEFINES):
+    if (lenKeyMin == -1)
+        lenKeyMin = MIN;
+    if (lenKeyMax == -1)
+        lenKeyMax = MAX;
+    if (!alphabet) {
+        alphabet = ALPHABET;
     }
 
-  // Miramos si queda algun parámetro sin caracter de opcion (es decir, un parametro suelto sin precedente). En este caso, deberia ser el hash.
-  if (optind + 1 == argc) { 
-    // hay hash!
-    ejemplo_diggest = argv[optind];
-    // Lo convertimos a UPPER CASE por si acaso:
-    int p = 0;
-    while (ejemplo_diggest[p] != '\0') {ejemplo_diggest[p] =  toupper(ejemplo_diggest[p]); p++;}
-  } else {
-    // Si no hay más parametros o hay más de uno, mostramos por pantalla una alerta al usuario.
-    printf("Error, usage: %s [-a \"ALPHABET\"] [-n MIN] [-m MAX] <hash>\n", argv[0]);
-    exit(EXIT_FAILURE); // no seguimos ejecutando
-  }
-
-  // Si el usuario no ha introducido los parametros opcionales y solo el hash como tal. Inicializamos a valores por defecto (DEFINES):
-  if (lenKeyMin == -1)
-	lenKeyMin = MIN;
-  if (lenKeyMax == -1)
-   	lenKeyMax = MAX;
-  if (!alphabet) {
-    	alphabet = ALPHABET;
-  }
-
-	printf("---------------\n");
-	printf("· alfabeto: %s\n", alphabet);
-	printf("· min: %d\n", lenKeyMin);
-	printf("· max: %d\n", lenKeyMax);
-	printf("· diggest: %s\n", ejemplo_diggest);
+    printf("---------------\n");
+    printf("· alfabeto: %s\n", alphabet);
+    printf("· min: %d\n", lenKeyMin);
+    printf("· max: %d\n", lenKeyMax);
+    printf("· diggest: %s\n", ejemplo_diggest);
 
     ///////// END GETOPT IMPLEMENTATION: //////////////////////////////////////////////
     // A continuacion comenzamos con la impementacion del cracker propiamente dicho:
@@ -109,49 +115,73 @@ int main(int argc, char *argv[]) {
     unsigned long long i = 0;
     int j;
     int l;
-	int rank, size, chunk_size, indice_comienzo;
+    int rank, chunk_size, indice_comienzo;
 
     //Private elements
     unsigned char *candidato; // Clave candidata generada a partir de un determinado alfabeto con longitud de clave dada.
     unsigned char *candidate_diggest; // Hash de la clave candidata
-    unsigned char buffer [65]; // Buffer auxiliar que utilizamos para formatear correctamente el hash que obtenemos de la clave candidata
+    unsigned char buffer[65]; // Buffer auxiliar que utilizamos para formatear correctamente el hash que obtenemos de la clave candidata
     int stop = 0; // "Booleano" que indica cuando parar de buscar.
-
-	// Paralelizando con MPI:
-		MPI_Init(&argc, &argv);
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int stopFinal = 0;
+    // Paralelizando con MPI:
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     // Generacion de TODAS las claves CANDIDATAS para una clave de un determinado tamaño comprendido entre MIN y MAX.
     // Hasheamos la clave candidata generada y la comparamos con el diggest introducido por el usuario por parametro.
     // Si coinciden: entonces encontrado! hemos crackeado la contrasena. SI NO, entonces nos vamos a ver una peli con palomitas.
-    for (j = lenKeyMin; j <= lenKeyMax&&!stop; j++) {
+
+    clock_t begin = clock();
+    for (j = lenKeyMin; j <= lenKeyMax && !stopFinal; j++) {
         keyspace = mypow(lenAlpha, j);
-        
-		
-		chunk_size = keyspace/size; // Cantidad de numeros que cada proceso ha de procesar, siendo 'Size' numero de procesos.
 
-		int k;
 
-		if (rank == 0) {
-			for (k=1; k<size;k++) {
-					int numero = chunk_size*k;
-					//printf("NUMERO ENVIADO! (master)\n");
-					MPI_Send(&numero, 1, MPI_UNSIGNED_LONG, k, 0, MPI_COMM_WORLD);
-					indice_comienzo = 0;
-			} 
-		} else {
-				// Para cualquier otro proceso (incluido el master) recogemos el indice de inicio (por el cual parte cada proceso) y continuamos normal:
-				MPI_Recv(&indice_comienzo,1,MPI_UNSIGNED_LONG,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD, &status);
-				printf("NUMERO RECIBIDO! (proceso#%d) | numero = %d\n", rank,indice_comienzo);
-		}
-		printf("%d | %d | %llu\n", indice_comienzo, chunk_size, i);
-        for (i = indice_comienzo; i < (indice_comienzo + chunk_size); i++) {
-			//printf("Soy proceso#%d, me ocupo del numero:%llu\n", rank, i);
-			
-            if(!stop) {
+        chunk_size = keyspace / size; // Cantidad de numeros que cada proceso ha de procesar, siendo 'Size' numero de procesos.
+
+        int k;
+
+        if (rank == 0) {
+            for (k = 1; k < size; k++) {
+                int numero = chunk_size * k;
+                //printf("NUMERO ENVIADO! (master)\n");
+                MPI_Send(&numero, 1, MPI_UNSIGNED_LONG, k, 0, MPI_COMM_WORLD);
+                indice_comienzo = 0;
+            }
+        } else {
+            // Para cualquier otro proceso (incluido el master) recogemos el indice de inicio (por el cual parte cada proceso) y continuamos normal:
+            MPI_Recv(&indice_comienzo, 1, MPI_UNSIGNED_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            printf("NUMERO RECIBIDO! (proceso#%d) | numero = %d\n", rank, indice_comienzo);
+        }
+        printf("%d | %d | %llu\n", indice_comienzo, chunk_size, i);
+
+        for (i = indice_comienzo; i < (indice_comienzo + chunk_size)&&!stopFinal; i++) {
+            //printf("Soy proceso#%d, me ocupo del numero:%llu\n", rank, i);
+            // Generamos candidato:
+            candidato = cambioBase(&alphabet[0], i, j);
+            // Hasheamos el candidato con nuestra funcion Hash:
+            candidate_diggest = sha256_hasher(candidato);
+            // Formateamos adecuadamente el hash del candidato obtenido para poder compararlo con el <hash> introducido por el usuario:
+            for (l = 0; l < 32; l++) {
+                sprintf(&buffer[2 * l], "%02X", candidate_diggest[l]);
+            }
+            if (strcmp(ejemplo_diggest, buffer) == 0) {
+                printf("Estas de ENHORABUENA! Match de diggests.\n· Diggest (hash): %s\n· Texto plano: %s\n",
+                       buffer, candidato);
+                stop = 1;
+            }
+            if(((clock()-begin)/CLOCKS_PER_SEC)>1){
+                MPI_Reduce(&stop, &stopFinal, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+                begin = clock();
+            }
+        }
+
+        // Ahora tratamos el resto:
+        if (rank == 0 && keyspace % size != 0) {
+            for (i = keyspace - (keyspace % size); i < keyspace&&!stopFinal; i++) {
                 // Generamos candidato:
                 candidato = cambioBase(&alphabet[0], i, j);
+
                 // Hasheamos el candidato con nuestra funcion Hash:
                 candidate_diggest = sha256_hasher(candidato);
                 // Formateamos adecuadamente el hash del candidato obtenido para poder compararlo con el <hash> introducido por el usuario:
@@ -162,42 +192,13 @@ int main(int argc, char *argv[]) {
                     printf("Estas de ENHORABUENA! Match de diggests.\n· Diggest (hash): %s\n· Texto plano: %s\n",
                            buffer, candidato);
                     stop = 1;
-                } else {
-                    //printf("Key: %s\n", candidato);
                 }
                 free(candidato); // evitamos que nuestra memoria RAM explote!
                 free(candidate_diggest); // evitamos que nuestra memoria RAM explote!
             }
         }
-
-		// Ahora tratamos el resto:
-		if (rank == 0 && keyspace%size!=0) {
-			for (i = keyspace-(keyspace%size); i < keyspace; i++) {
-				printf("Soy proceso#%d, me ocupo del numero:%llu\n", rank, i);
-				
-		        if(!stop) {
-		            // Generamos candidato:
-		            candidato = cambioBase(&alphabet[0], i, j);
-		            // Hasheamos el candidato con nuestra funcion Hash:
-		            candidate_diggest = sha256_hasher(candidato);
-		            // Formateamos adecuadamente el hash del candidato obtenido para poder compararlo con el <hash> introducido por el usuario:
-		            for (l = 0; l < 32; l++) {
-		                sprintf(&buffer[2 * l], "%02X", candidate_diggest[l]);
-		            }
-		            if (strcmp(ejemplo_diggest, buffer) == 0) {
-		                printf("Estas de ENHORABUENA! Match de diggests.\n· Diggest (hash): %s\n· Texto plano: %s\n",
-		                       buffer, candidato);
-		                stop = 1;
-		            } else {
-		                //printf("Key: %s\n", candidato);
-		            }
-		            free(candidato); // evitamos que nuestra memoria RAM explote!
-		            free(candidate_diggest); // evitamos que nuestra memoria RAM explote!
-		        }
-		    }
-		}
     }
 
-	MPI_Finalize();
+    MPI_Finalize();
     return 0;
 }
